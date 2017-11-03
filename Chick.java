50b18b339c9531d1504f7009bff0221f65e14273
belee3 <31221162+belee3@users.noreply.github.com>
2017-11-03 10:34:30 -0700

    Update Chick.java

--- Chick.java
+++ Chick.java
@@ -1,4 +1,14 @@
-class Chick //implements Animal 
-{     
-     //your code here
+class Chicken implements Animal{
+  private String myType;
+  private String mySound;
+  public Chicken(String type, String sound){
+    myType = type;
+    mySound = sound;
+  }
+  public Chicken(){
+    myType = "Chicken";
+    mySound = "Bok";
+  }
+  public String getSound(){return mySound;}
+  public String getType(){return myType;}
 }
